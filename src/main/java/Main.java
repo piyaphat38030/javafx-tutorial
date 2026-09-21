7542051f4272ab44cb86db9d805aa0f55564efb6
Piyaphat <piyaphat38030@gmail.com>
2026-09-21 22:59:16 +0800

    JavaFX: Build tutorial chat layout

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,31 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /** A minimal JavaFX window. */
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        stage.setScene(new Scene(new Label("Hello World!")));
+        ScrollPane scrollPane = new ScrollPane(new VBox());
+        TextField userInput = new TextField();
+        Button sendButton = new Button("Send");
+        AnchorPane layout = new AnchorPane(scrollPane, userInput, sendButton);
+        AnchorPane.setTopAnchor(scrollPane, 0.0);
+        AnchorPane.setBottomAnchor(scrollPane, 42.0);
+        AnchorPane.setLeftAnchor(scrollPane, 0.0);
+        AnchorPane.setRightAnchor(scrollPane, 0.0);
+        AnchorPane.setBottomAnchor(userInput, 0.0);
+        AnchorPane.setLeftAnchor(userInput, 0.0);
+        AnchorPane.setRightAnchor(userInput, 76.0);
+        AnchorPane.setBottomAnchor(sendButton, 0.0);
+        AnchorPane.setRightAnchor(sendButton, 0.0);
+        stage.setTitle("Duke");
+        stage.setScene(new Scene(layout, 400, 600));
         stage.show();
     }
 }
